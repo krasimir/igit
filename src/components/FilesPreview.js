d7ccc7317dfe130a668edfe0ba77fb2fe4b74c04
jmrapp1 <jmrapp1270@gmail.com>
2019-04-30 16:13:27 -0400

    Fixed page scrolling issue

--- src/components/FilesPreview.js
+++ src/components/FilesPreview.js
@@ -4,23 +4,31 @@ import PropTypes from 'prop-types';
 
 import { CHEVRON_RIGHT, CHEVRON_DOWN } from './Icons';
 import Diff from './utils/Diff';
+import { Link } from 'react-router-dom';
 
 const INDENT_STEP = 4;
 
-function Directory({ dir, indent }) {
+function Directory({ dir, indent, url }) {
   const isFile = dir.items === null;
+
+  const inFileView = url.match(/\/files$/) !== null;
 
   return (
     <div style={ { paddingLeft: `${ indent }px` } } className={ isFile ? 'is-file' : 'is-dir' }>
-      { isFile ? <a href={ `#${ dir.fullPath }` }>{ dir.path }</a> : dir.path }
+      { !isFile ?
+          dir.path :
+          inFileView ?
+              <a href={ `#${ dir.fullPath }` }>{ dir.path }</a> :
+              <Link to={ `${url}/files#${ dir.fullPath }` }>{ dir.path }</Link>
+      }
       { isFile ?
         <Diff data={ { additions: dir.additions, deletions: dir.deletions } } /> : '' }
-      { !isFile && dir.items.map(item => <Directory dir={ item } indent={ indent + INDENT_STEP } key={ item.path }/>)}
+      { !isFile && dir.items.map(item => <Directory url={ url } dir={ item } indent={ indent + INDENT_STEP } key={ item.path }/>)}
     </div>
   );
 }
 
-export default function FilesPreview({ pr }) {
+export default function FilesPreview({ pr, url }) {
   const [ expanded, expand ] = useState(false);
 
   return (
@@ -33,7 +41,7 @@ export default function FilesPreview({ pr }) {
         expanded && (
           <div className='pl1 bl1 ml1 files-preview'>
             {
-              pr.files.tree.items.map(item => <Directory dir={ item } indent={ INDENT_STEP } key={ item.path } />)
+              pr.files.tree.items.map(item => <Directory url={ url } dir={ item } indent={ INDENT_STEP } key={ item.path } />)
             }
           </div>
         )
